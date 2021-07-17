12806f551eb7ae24fd732ee0ac087b1fe6d5c296
kunal <singalkunal10@gmail.com>
2021-07-18 00:22:16 +0530

    stringify firebase service account object

--- index.js
+++ index.js
@@ -149,16 +149,16 @@ const startUp = async () => {
     // connect Firebase Storage
     admin.initializeApp({
         credential: admin.credential.cert({
-        type,
-        project_id,
-        private_key_id,
-        private_key,
-        client_email,
-        client_id,
-        auth_uri,
-        token_uri,
-        auth_provider_x509_cert_url,
-        client_x509_cert_url
+        "type": type,
+        "project_id": project_id,
+        "private_key_id": private_key_id,
+        "private_key": private_key,
+        "client_email": client_email,
+        "client_id": client_id,
+        "auth_uri": auth_uri,
+        "token_uri": token_uri,
+        "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
+        "client_x509_cert_url": client_x509_cert_url
     }),
         storageBucket: process.env.BUCKET_URL
     });
