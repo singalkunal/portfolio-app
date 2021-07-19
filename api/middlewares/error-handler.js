87f0b362ddcf0f45aa566d9d4a8fcbc2313789dd
kunal <singalkunal10@gmail.com>
2021-07-19 16:47:50 +0530

    init client

--- api/middlewares/error-handler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    if(err.name && err.name === 'custom-error') {
-        return res.status(err.statusCode).send({errors: err.serializeErrors()});
-    }
-
-    res.status(500).send({errors: [{msg: 'Some error happened...'}]});
-}
-
-module.exports = errorHandler;
